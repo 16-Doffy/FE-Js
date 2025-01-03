f73712677ea1852b09502a4fe132158e5cff7517
Nhut Duy <ngophamnhutduy050302@gmail.com>
2025-01-03 21:56:28 +0700

    rs slide img

---
+++ src/YutData.js
@@ -0,0 +1,28 @@
+import s1 from "./img/s1.jpg";
+import s2 from "./img/s2.jpg";
+import s3 from "./img/s3.jpg";
+import s4 from "./img/s4.jpg";
+import s5 from "./img/s5.jpg";
+export const YtData = [
+  {
+    id: 1,
+    title: "idol",
+    auth: "Sayuri",
+    image: s1,
+    avatar: s2,
+  },
+  {
+    id: 2,
+    title: "singer",
+    auth: "Sayuri",
+    image: s3,
+    avatar: s4,
+  },
+  {
+    id: 3,
+    title: "singel moms",
+    auth: "Sayuri",
+    image: s4,
+    avatar: s5,
+  },
+];
